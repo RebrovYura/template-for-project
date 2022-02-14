9f74c9ef0688eb97f1d77ffa7ef85092667a08f8
Yura <yurirebrov01@gmail.com>
2022-02-14 13:56:54 +0300

    create base, header

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+    if(toggle && nav) {
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu');
+        })
+    }
+}
+showMenu('nav-burger', 'nav-menu');
+
+const navLink = document.querySelectorAll('.header__nav-link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(el => el.addEventListener('click', linkAction));
