0a70a56b74a337ed48eaa088b4723cf6a5f188ad
Yuri Rebrov <yurirebrov01@gmail.com>
2022-02-21 13:41:02 +0300

    Sign in and sign up pages template created

--- src/js/script.js
+++ src/js/script.js
@@ -20,21 +20,9 @@ function linkAction() {
 navLink.forEach(el => el.addEventListener('click', linkAction));
 
 
-// const signin = document.querySelector('.sign-in');
-// const signup = document.querySelector('.sign-up');
+// const signinBtn = document.querySelector('.sign-in');
 // const signinForm = document.querySelector('.signin-form');
-// const signupForm = document.querySelector('.signup-form');
+// signinBtn.addEventListener('click', function signForms() {
+//     console.log('skdjlskd');
 
-// function showForm() {
-//     if (signin.addEventListener('click')) {
-//         signupForm.classList.add('.hide-form');
-//         signupForm.classList.remove('.hide-form');
-//     }
-//     if (signup.addEventListener('click')) {
-//         signinForm.classList.add('.hide-form');
-//         signupForm.classList.remove('.hide-form');
-//     }
-//     console.log(signin.addEventListener('click'));
-// }
-
-// showForm();
+// });
